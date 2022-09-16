39d8c9b8d230a0c2d7395d9b2faa0cf991eca753
3mrotaha <tamr4512@gmail.com>
2022-09-16 14:18:51 +0200

    card module finished but not tested yet

--- Card/card.c
+++ Card/card.c
@@ -4,8 +4,67 @@
 #include "card.h"
 
 
-EN_cardError_t getCardHolderName(ST_cardData_t* cardData);
+EN_cardError_t getCardHolderName(ST_cardData_t* cardData){
+    // reading the name from user with fgets to read string with spaced
+    // there're different methods to do the same thing too with scanf and gets functions
+    fgets(cardData->cardHolderName, HOLDER_MAX_CHAR, stdin);
+    // check the string requirements in length and non-Null pointer
+    if(checkStringValidation(cardData->cardHolderName, HOLDER_MAX_CHAR, HOLDER_MIN_CHAR) == VALID)
+        return OK; // the string entered by user meets the holder name requirements
+    
+    return WRONG_NAME; // the string entered does NOT meet holder name requiements
+}
 
-EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
+EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData){
+    // reading the name from user with fgets to read string with spaced
+    // there're different methods to do the same thing too with scanf and gets functions
+    fgets(cardData->cardExpirationDate, HOLDER_MAX_CHAR, stdin);
+    // check the string requirements in length and non-Null pointer
+    if(checkStringValidation(cardData->cardExpirationDate, HOLDER_MAX_CHAR, HOLDER_MIN_CHAR) == VALID){
+        // after that we have to check the format of the date as well
+        if(checkDateFormat(cardData->cardExpirationDate) == VALID)
+            return OK; //  string entered by user meets the PAN requirements
+    }
+    
+    return WRONG_EXP_DATE; // the string entered does NOT meet the PAN requiements
+}
 
-EN_cardError_t getCardPAN(ST_cardData_t* cardData);
+EN_cardError_t getCardPAN(ST_cardData_t* cardData){    
+    // reading the PAN from user with fgets to read string with spaced
+    // there're different methods to do the same thing too with scanf and gets functions
+    fgets(cardData->primaryAccountNumber, HOLDER_MAX_CHAR, stdin);
+    // check the string requirements in length and non-Null pointer
+    if(checkStringValidation(cardData->primaryAccountNumber, HOLDER_MAX_CHAR, HOLDER_MIN_CHAR) == VALID)
+        return OK; //  string entered by user meets the PAN requirements
+    
+    return WRONG_PAN; // the string entered does NOT meet the PAN requiements
+}
+
+
+static uint8_t checkStringValidation(uint8_t string[], uint8_t maxlen, uint8_t minlen){
+    if(string != NULL && strlen(string) >= minlen && strlen(string) < maxlen){
+        // string is valid so we return a valid value
+        return VALID; // definintion in global.h
+    }
+
+    return INVALID; // definition in global.h
+}
+
+static uint8_t checkDateFormat(uint8_t date[]){
+    // checking that date has the format MM/YY
+    for(uint8_t i = 0; i < EXDATE_MAX_CHAR; i++){
+        if(i != 2){
+            if(!(date[i] >= '0' && date[i] <= '9'))
+                return INVALID; // format contain a non digit character in MM or YY
+        }
+        else{
+            if(date[i] != '/')
+                return INVALID; // character that separates MM from YY is not /
+        }
+    }
+    // checking that month is not greater than 12
+    if(date[0] > '1' || (date[0] == '1' && date[1] > '2'))
+        return INVALID;
+
+    return VALID;
+}
--- Card/card.h
+++ Card/card.h
@@ -4,11 +4,16 @@
 #include "../global.h"
 
 #define HOLDER_MAX_CHAR     25 /* Holder name max allowed string length */
-#define PAN_MAX_CHAR        20 /* Account Number max allowed string length */
+#define HOLDER_MIN_CHAR     20 /* Holder name min allowed string length */
+
+#define PAN_MAX_CHAR        20 /* Account number max allowed string length */
+#define PAN_MIN_CHAR        16 /* Account number min allowed string length */
+
 #define EXDATE_MAX_CHAR     6  /* Expiry date max allowed string length */
+#define EXDATE_MAX_CHAR     5  /* Expiry date max allowed string length */
 
 /* 
- * the following enum defines the error check type for
+ * This enum defines the error check type for
  * Card module
 */
 typedef enum EN_cardError_t{
@@ -19,7 +24,7 @@ typedef enum EN_cardError_t{
 }EN_cardError_t;
 
 /* 
- * the following struct defines the card container
+ * This struct defines the card container
  *  and its members
  */
 typedef struct ST_cardData_t{
@@ -35,10 +40,36 @@ typedef struct ST_cardData_t{
  *       allowed to be used card.c only
  */
 
+
+// this function asks the user to enter the card holder name
+// returns:
+// OK: if the name length is between 20 and 24 and not NULL
+// WRONG_NAME: if the name is NULL or its length not match required length
 EN_cardError_t getCardHolderName(ST_cardData_t* cardData);
 
+// this function asks the user to enter the card expiry date
+// returns:
+// OK: if the date length is between 20 and 24 and not NULL and has a correct format MM/YY
+// WRONG_EXP_DATE: if the date is NULL or its length not match required length or format is not MM/YY
 EN_cardError_t getCardExpiryDate(ST_cardData_t* cardData);
 
+// this function asks the user to enter the card PAN
+// returns:
+// OK: if the PAN length is between 20 and 24 and not NULL
+// WRONG_PAN: if the PAN is NULL or its length not match required length
 EN_cardError_t getCardPAN(ST_cardData_t* cardData);
+
+// this HELPER function is used in card module to check the format
+// of the date entered by the user and date should has the format MM/YY
+// and monthes should be less than 12
+static uint8_t checkDateFormat(uint8_t date[]);
+
+// this HELPER function is used in the card module to check
+// if the string entered by the user meets the specifications required
+// or not, it checks the length correctness and that string is not NULL
+// returns:
+// VALID: if the string meets the specifications
+// INVALID: if the string does NOT meet specifications
+static uint8_t checkStringValidation(uint8_t string[], uint8_t maxlen, uint8_t minlen);
 
 #endif
