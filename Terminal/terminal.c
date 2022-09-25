0fab004cd624d51fe1e64361c55aa72820d0fe88
3mrotaha <tamr4512@gmail.com>
2022-09-25 19:56:32 +0200

    terminal amt first commit

--- Terminal/terminal.c
+++ Terminal/terminal.c
@@ -28,6 +28,7 @@ EN_terminalError_t getTransactionDate(ST_terminalData_t *termData)
 
 }
 
+<<<<<<< HEAD
 // Function to check if the date entered by the user is valid or not
 uint8_t Check_Term_Date(uint8_t date)
 {
@@ -36,5 +37,15 @@ uint8_t Check_Term_Date(uint8_t date)
     
     else
          return VALID;   //return 1
+=======
+EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termData){
+    // comapare the card expiry date with the date of today
+    // it should be less than the date of transaction
+    if()
+>>>>>>> 70bf4eb (terminal amt first commit)
 }
 
+static uint8_t compareDates(uint8_t exDate[], uint8_t TransDate[]){
+       
+    if()
+}
--- Terminal/terminal.h
+++ Terminal/terminal.h
@@ -2,10 +2,14 @@
 #define _TERMINA_H_
 
 #include "../global.h"
+<<<<<<< HEAD
 
 #define DATE_LENTH 10   // The valid length of Date
 
 EN_terminalError_t getTransactionDate(ST_terminalData_t *termData);
+=======
+#include "../Card/card.h"
+>>>>>>> 70bf4eb (terminal amt first commit)
 
 typedef struct ST_terminalData_t
 {
@@ -17,7 +21,23 @@ typedef struct ST_terminalData_t
 
 typedef enum EN_terminalError_t
 {
-    OK,WRON_DATE,EXPIRED_CARD,INVALID_CARD,INVALID_AMOUNT,EXCEED_MAX_AMOUNT,INVALID_MAX_AMOUNT
+    OK,WRON_DATE,
+    EXPIRED_CARD,
+    INVALID_CARD,
+    INVALID_AMOUNT,
+    EXCEED_MAX_AMOUNT,
+    INVALID_MAX_AMOUNT
 }EN_terminalError_t;
 
+EN_terminalError_t getTransactionDate(ST_terminalData_t *termData);
+
+EN_terminalError_t isCardExpired(ST_cardData_t cardData, ST_terminalData_t termData);
+
+/*
+this function compares the date on the card with the date of transaction
+it returns:
+VALID: if the date on the card is greater than the date of transaction
+INVALID: if the date is less than the date of transaction
+*/
+static uint8_t compareDates(uint8_t exDate[], uint8_t TransDate[]);
 #endif
