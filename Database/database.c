62e56074c065c2730bb9e36ad119f22843b60672
3mrotaha <tamr4512@gmail.com>
2022-09-29 07:21:38 +0200

    added a function to map file path

--- Database/database.c
+++ Database/database.c
@@ -20,3 +20,13 @@ uint8_t searchInDatabase(uint8_t PAN[]){
 
     return INVALID; // pan is not in the database
 }
+
+uint8_t mapUserFilePath(uint8_t holderName[], uint8_t PAN[], uint8_t Path[]){
+    strcpy(Path, holderName);
+    strcat(Path, PAN);
+    strcat(Path, DATABASE_FILE_EXTENSION);
+
+    return strlen(Path);    // return the path for any usage in future functions
+                            // it also could be a check mechanism as the path len must be 
+                            // greater than the len of the holderName + PAN passed to this function
+}
--- Database/database.h
+++ Database/database.h
@@ -4,14 +4,17 @@
 #include "../global.h"
 #include "../Server/server.h"
 
-#define FILE_PATH       "data.txt" // here we configure the file bath
-#define CREATE_FILE          "a"   // use this flag in fopen to create the file
-#define READ_FROM_FILE       "r"   // use this falg in fopen to read from file
-#define WRITE_TO_FILE        "w"   // use this flag in fopen to write to a file
-#define READ_WRITE           "rw"  // this flag is used in functions that needs to read and write in the same time
+#define FILE_PATH       "db/data"       // here we configure the file bath
+#define DATABASE_FILE_EXTENSION ".txt"  // the extension of all files created in the database
+#define CREATE_FILE          "a"        // use this flag in fopen to create the file
+#define READ_FROM_FILE       "r"        // use this falg in fopen to read from file
+#define WRITE_TO_FILE        "w"        // use this flag in fopen to write to a file
+#define READ_WRITE           "rw"       // this flag is used in functions that needs to read and write in the same time
 
-#define MAX_PAN              100
-#define VALID_DATA_INPUT     1
+
+#define FILE_PATH_MAX_LENGTH    100     // max length of the file path in the database
+#define MAX_PAN              100        // max pan characters
+#define VALID_DATA_INPUT     1          // for checking validity of the data
 /*
 this function is used to search about a user in the database using 
 its pinary account number
@@ -22,6 +25,16 @@ INVALID: if the PAN is not in the database
 */
 uint8_t searchInDatabase(uint8_t PAN[]);
 
+
+/*this function is used to map a path for the user file in database
+  the file created is NamePan.Extension i.e Menna1234564891011.txt
+  
+  returns:
+    length of the path mapped
+*/
+uint8_t mapUserFilePath(uint8_t holderName[], uint8_t PAN[], uint8_t Path[]);
+
+
 /*
 this function is used to insert user info into database
 
@@ -31,10 +44,10 @@ DB_ERROR: if data was not inserted successfully
 */
 uint8_t insertToDatabase(uint8_t PAN[], f32_t balance);
 
+
 /*
 this function is used to get the user palance for 
 already existed user in the database
-
 returns: 
 USER_BALANCE_OK: if the user has a valid balance in the data base
                  i.e (not a 0 balance)
@@ -43,12 +56,14 @@ DB_ERROR: if the user has invalid balance in the database
 */
 uint8_t getBalance(uint8_t PAN[], f32_t* balance);
 
+
 /*
 this function is used to open the data base file it's not used outside the database 
 module so we declared it as static
 returns: void
 */
 static void openfile(uint8_t filebath[], uint8_t flag[]);
+
 
 /*
 this function is used to close the opened data base file 
