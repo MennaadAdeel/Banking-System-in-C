6995d2f44d52d9e9bace7982c4d540a98d36cbe6
3mrotaha <tamr4512@gmail.com>
2022-10-08 11:45:43 +0200

    not important

--- Server/server.h
+++ Server/server.h
@@ -44,9 +44,17 @@ typedef struct ST_accountDB_t
 
 
 EN_transState_t recieveTransactionData(ST_transaction_t *transData);
+
+
 EN_serverError_t isValidAccount(ST_cardData_t *cardData);
+
+
 EN_serverError_t isAmountAvailable(ST_terminalData_t *termData);
+
+
 EN_serverError_t saveTransaction(ST_transaction_t *transData);
+
+
 EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t *transData);
 
 
