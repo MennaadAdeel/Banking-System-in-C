c0b35a6051ffc101ac49d81e597b8778d4481a86
Menna Adel <105021510+MennaadAdeel@users.noreply.github.com>
2022-10-09 21:21:59 +0200

    edited DataBase.h

--- Database/database.h
+++ Database/database.h
@@ -13,10 +13,10 @@ typedef enum EN_DatabaseError_t{
 
 
 /* This finction is used to write data into DataBase */
-EN_DatabaseError_t writeData(ST_accountDB_t *Blance, ST_accountDB_t *PAN);
+EN_DatabaseError_t writeData(ST_accountDB_t *data);
 
 /* This finction is used to read data into DataBase */
-EN_DatabaseError_t readData(ST_accountDB_t *Blance, ST_accountDB_t *PAN);
+EN_DatabaseError_t readData(ST_accountDB_t *data;
 
 /* This finction is used to search data into DataBase */
 EN_DatabaseError_t searchData(ST_cardData_t *PAN );
