8cbaa11e0896ddfeb0cbeb895674d74070d57f45
3mrotaha <tamr4512@gmail.com>
2022-10-14 14:04:17 +0200

    added log functions, provided comments on some functions

--- Database/database.h
+++ Database/database.h
@@ -5,13 +5,12 @@
 #define READ_FORMAT "%s &%f;\n"
 #define WRITE_FORMAT  "%s &%f;          "
 #define MAX_READ_WRITE_CHAR 100
-typedef enum EN_DatabaseError_t{
 
+typedef enum EN_DatabaseError_t{
   OK,
   READ_WRITE_ERROR,
   USER_NOT_FOUND,
-  ERROR_FILE
-  
+  ERROR_FILE  
 }EN_DatabaseError_t;
 
 
@@ -27,11 +26,28 @@ EN_DatabaseError_t readData(ST_accountDB_t *accData);
 /* This finction is used to search data into DataBase */
 EN_DatabaseError_t searchData(ST_accountDB_t *accData);
 
+/* this function will save all the transaction data into 
+   a file called log in database
+*/
+EN_DatabaseError_t saveLog(ST_transaction_t transData);
 
+/*
+  this function will get a transaction saved to the log
+  file before (the key is the sequence number)  
+*/
+EN_DatabaseError_t getLog(ST_transaction_t *transData);
 
+// ======================= HELPER FUNCTIONS ===============================
 
-
+/*
+this function gets the position in which 
+the pan of the card exists
+*/
 static inline fpos_t getLineIndex(uint8_t pan[]);
 
+/*
+this function closes the file opened 
+and assigns the pointer to that file to NULL
+*/
 static inline void closeFile(void);
 #endif
